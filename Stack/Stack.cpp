79d0210082f72727a653e66e0e30ea5c05945ea6
Alcatraz Lynn <lym0104@126.com>
2019-02-05 17:58:41 +0800

    New Code with Stack Question

---
+++ Stack/Stack.cpp
@@ -0,0 +1,61 @@
+#include "Stack.h"
+
+int getLength(char *a){
+    int len;
+    len = strlen(a);
+    return len;
+}
+
+int getMid(int length){
+    if(length % 2 == 0){
+        return length / 2;
+    }
+    else{
+        return length / 2 - 1;
+    }
+}
+
+int getNext(int length){
+    if(length % 2 == 0){
+        return length / 2;
+    }
+    else{
+        return length / 2 + 1;
+    }
+}
+
+bool isPalindrome(int top){
+    if(top == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main()
+{
+    char a[100];
+    gets(a);
+
+    int len = 0, next = 0, top = 0;
+    len = getLength(a);
+    top = getMid(len);
+    next = getNext(len);
+    for (int i = next; i < len; i++){
+        if(a[next] != a[top])
+            break;
+        top--;
+    }
+
+    bool isPal = false;
+    isPal = isPalindrome(top);
+    
+    if(isPal)
+        cout << "Is Palindrome!" << endl;
+    else
+        cout << "Not Palindrome!" << endl;
+    getchar();
+    getchar();
+    return 0;
+}
---
+++ Stack/Stack.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int getLength(char *string);
+int getMid(int length);
+int getNext(int length);
+bool isPalindrome(int top);
