81472382e3dc00cbb15d44c239df4d2c1f02ab3a
Alcatraz Lynn <lym0104@126.com>
2019-05-05 20:10:01 +0800

    finish Q2-2

---
+++ List/Q2/2-2.h
@@ -0,0 +1,11 @@
+template <class T>
+void ArrayList<T> :: DeleteSpec(T x){
+    int i = 0;
+    for (i; i < curLen; i++){
+        if(ArrayList[i] = x){
+            for (int j = i; j < curLen - 1; j++)
+                ArrayList[j] = ArrayList[j + 1];
+            curLen--;
+        }
+    }
+}
