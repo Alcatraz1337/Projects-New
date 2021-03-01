760abbc8b4186ddd33517038f62e6316e8e3437f
Alcatraz Lynn <lym0104@126.com>
2021-03-01 14:24:13 +0900

    Update

--- test.c
+++ test.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct 
+{
+    int x, y;
+}player;
+
 
 int main(){
-    double x = 1.9;
-    int y = 3;
-    int z = x + y;
-    printf("z = x + y = %d", z);
+    player p1, p2;
+    p1.x = 1;
+    p1.y = 2;
+    p2.x = 3;
+    p2.y = 4;
+    p2 = p1;
+    printf("P2x = %d, P2y = %d", p2.x, p2.y);
     return 0;
 }
